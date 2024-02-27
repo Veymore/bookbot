65fb3cb3b7c797e0c7e132940ff409a6a12d9cc0
veymore <julian.karl90@gmail.com>
2024-02-28 00:03:56 +0100

    read file from hardcoded path
    - wrote a function to read a book as file
    - convert _io.TextIOWrapper to string with read()
    - print string

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("hello world")
+repository_path = "/Users/julian/Desktop/repos/boot.dev/bookbot/"
+path_to_file = "books/frankenstein.txt"
+def print_file(path):
+    f = open(path, "r", encoding="utf-8")
+    print(f.read())
+
+def main():
+    print("debug main:")
+    print_file(repository_path + path_to_file)
+
+if __name__ == "__main__":
+    main()
