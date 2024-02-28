39dda2ee0714ecd766b4c11bd5b318d8ecf6752a
veymore <julian.karl90@gmail.com>
2024-02-28 19:39:35 +0100

    refactor main file
    - restructured helper functions
    - simplified main call
    - reduced redundant file reading calls

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
-repository_path = "/Users/julian/Desktop/repos/boot.dev/bookbot/"
-path_to_file = "books/frankenstein.txt"
-def print_file(path):
-    f = open(path, "r", encoding="utf-8")
-    print(f.read())
+def read_file_to_String(path):
+    return open(path, "r", encoding="utf-8").read()
 
+def count_words(text):
+    return len(text.split())
+    
 def main():
-    print("debug main:")
-    print_file(repository_path + path_to_file)
+    repository_path = "/Users/julian/Desktop/repos/boot.dev/bookbot/"
+    path_to_file = "books/frankenstein.txt"
+    book_path = repository_path + path_to_file
+    book_text = read_file_to_String(book_path)
+    print(count_words(book_text))
 
-if __name__ == "__main__":
-    main()
+main()
