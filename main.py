7f48685cfb8085d8c7eee0c9d839c5eeb920b5e7
veymore <julian.karl90@gmail.com>
2024-02-28 20:39:38 +0100

    added print_report function

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ def count_words(text):
 Action:     Takes a String, iterates over every character
             saves and counts the number to occurences per character
 In:         text -> String
-Out:        Integer
+Out:        Dictionary('char': 'number of occurences')
 '''
 def count_letter(text):
     text = text.lower()
@@ -34,6 +34,41 @@ def count_letter(text):
     return letter_dict
 
 '''
+Action:     Calls count_letter, 
+            Sorts Dictionary,
+            Iterates over given Dictionary.
+            Filters characters that are not in the alphabet.
+            Prints a report of character occurences in human readable format.
+In:         text -> String
+Out:        -
+'''
+def print_report(text, book_path='undefined'):
+    report_begin =  f"""
+{'-'*3} Begin report of {book_path} {'-'*3}
+{count_words(text)} words found in the document
+
+"""
+    report_end = f"{'-'*3} End report {'-'*3}"
+    full_report = report_begin
+    #Call counter_letter
+    # -> use sorted() to create a sorted list
+    # -> use items() to convert dictionary into a list of key-value tuples
+    # -> use lambda x:x[1] to use the 2nd value of the k-v tuples
+    # -> use reverse to sort in ascending order
+    character_sorted_list = sorted(count_letter(text).items(), key=lambda x:x[1], reverse=True)
+    #Convert back to a dictionary
+    character_sorted_dict = dict(character_sorted_list)
+    #Iterate over given Dictionary
+    for char in character_sorted_dict:
+        #Filter characters that are not in the alphabet
+        if char.isalpha():
+            full_report += (f"The '{char}' character was found {character_sorted_dict[char]} times\n")
+    full_report += report_end
+    #Prints a report of character occurences in human readable format.
+    print(full_report)
+
+
+'''
 Action:     Main function, dictates the flow of the program
 In:         -
 Out:        -
@@ -43,7 +78,8 @@ def main():
     path_to_file = "books/frankenstein.txt"
     book_path = repository_path + path_to_file
     book_text = read_file_to_String(book_path)
-    count_letter(book_text)
+    #count_letter(book_text)
+    print_report(book_text, path_to_file)
 
 ## dont edit below ##
 
